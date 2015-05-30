03c0ece8786569ba45c8b45999f36bb050215a72
Carlos Gómez <cargomo@orxatastudios.com>
2015-05-30 20:43:56 +0200

    pene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,11 +8,6 @@ public class GameManager : osSingleton<GameManager> {
     public GameManager _manager;
 
     #region Handlers
-    public ManagerNavPoint _managerNavPoint;
-    public ManagerNavPoint ManagerNavPoint {
-        get { return _managerNavPoint; }
-    }
-
     public LifeManager _lifeManager;
     public LifeManager LifeManager {
         get { return _lifeManager; }
