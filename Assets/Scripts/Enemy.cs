d58a4c065ce75c779a2f94009c4e4d06e0b08322
Ignasi <ignasisal@orxatastudios.com>
2015-05-31 15:10:13 +0200

    Jefe quasi

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Enemy : Characters {
 
-    public Transform Left, Right;
+    public Transform Left, Right, Khame;
     public Transform arrow;
     public Enums.enemy_type currentType;
     public int attackValue;
@@ -38,15 +38,25 @@ public class Enemy : Characters {
     }
 
     public void Update () {
-        if ( alive && currentType != Enums.enemy_type.BOWMAN )
+        if ( alive && currentType != Enums.enemy_type.BOWMAN && currentType != Enums.enemy_type.KING )
             UpdatePosition ();
         else if ( alive && currentType == Enums.enemy_type.BOWMAN )
-            UpdateBowman ();
+            ;
+        //UpdateBowman ();
         else if ( alive && currentType == Enums.enemy_type.KING ) {
-            print ( "Hay un pene para Laura" );
             UpdateKing ();
+            MonarchyAttack ();
         } else
             Destroy ( this.gameObject );
+    }
+
+    public void MonarchyAttack () { 
+        float x = transform.position.x - GameManager.Instance.Knight.transform.position.x;
+        float y = transform.position.y - GameManager.Instance.Knight.transform.position.y;
+        if ( ( x < 8 || x > -8 ) && ( y < 8 || y > -8 ) ) {
+            currentInputState = Enums.inputState_nm.ATTACK;
+        } else
+            currentInputState = Enums.inputState_nm.WALK;
     }
 
     public void UpdateKing () {
@@ -54,26 +64,26 @@ public class Enemy : Characters {
         float x = GameManager.Instance.Knight.transform.position.x;
         float y = GameManager.Instance.Knight.transform.position.y;
         if ( ( transform.position.x >= Left.position.x ) && ( transform.position.x <= Left.position.x + 5 ) ) {
-            Vector3 aux = new Vector3 ( Right.position.x, GameManager.Instance.Knight.transform.position.y * 3, Right.position.z );
+            Vector3 aux = new Vector3 ( Right.position.x, GameManager.Instance.Knight.transform.position.y + 10, Right.position.z );
             transform.position = Vector3.MoveTowards ( transform.position, aux, walkVel * Time.deltaTime );
             toLeftKing = false;
         } else if ( ( transform.position.x <= Right.position.x ) && ( transform.position.x >= Right.position.x - 5 ) ) {
-            Vector3 aux = new Vector3 ( Left.position.x, GameManager.Instance.Knight.transform.position.y * 3, Right.position.z );
+            Vector3 aux = new Vector3 ( Left.position.x, GameManager.Instance.Knight.transform.position.y + 10, Right.position.z );
             transform.position = Vector3.MoveTowards ( transform.position, aux, walkVel * Time.deltaTime );
             toLeftKing = true;
         } else {
             if ( toLeftKing ) {
-                Vector3 aux = new Vector3 ( Left.position.x, GameManager.Instance.Knight.transform.position.y * 3, Right.position.z );
+                Vector3 aux = new Vector3 ( Left.position.x, GameManager.Instance.Knight.transform.position.y + 10, Right.position.z );
                 transform.position = Vector3.MoveTowards ( transform.position, aux, walkVel * Time.deltaTime );
             } else {
-                Vector3 aux = new Vector3 ( Right.position.x, GameManager.Instance.Knight.transform.position.y * 3, Right.position.z );
+                Vector3 aux = new Vector3 ( Right.position.x, GameManager.Instance.Knight.transform.position.y + 10, Right.position.z );
                 transform.position = Vector3.MoveTowards ( transform.position, aux, walkVel * Time.deltaTime );
             }
         }
     }
 
     // VA MAL
-    public void UpdateBowman () {
+    /*public void UpdateBowman () {
         float x = transform.position.x - GameManager.Instance.Knight.transform.position.x;
         float y = transform.position.y - GameManager.Instance.Knight.transform.position.y;
         if ( ( x < 12 || x > -12 ) && ( y < 12 || y > -12 ) ) {
@@ -86,7 +96,7 @@ public class Enemy : Characters {
                 float angle = Mathf.Atan2 ( ry, rx ) * Mathf.Rad2Deg;
                 transform.rotation = Quaternion.AngleAxis ( 90.0f - angle, Vector3.forward );
                 InvokeRepeating ("fletxes", 0.5f, 1.0f);
-                /*Vector3 aux2 = new Vector3 ();
+                Vector3 aux2 = new Vector3 ();
                 // ACÍ HI HA QUE INVOCAR UNA FLETXA I LLANÇAR-LA, ESPERAR UNS SEGONS I TORNAR A REPETIR MENTRE L'OBJECTIU ESTIGA A 12 DE DISTÀNCIA
                 if ( rx > transform.position.x && ry > transform.position.y )
                     aux2 = new Vector3 ( transform.position.x + 1, transform.position.y + 1, 0 );
@@ -98,10 +108,10 @@ public class Enemy : Characters {
                     aux2 = new Vector3 ( transform.position.x - 1, transform.position.y - 1, 0 );
                 Instantiate (arrow, aux2, transform.rotation);
                 arrow.rigidbody2D.AddForce (new Vector2(rx,ry));
-                StartCoroutine ( "arrowDelay" );*/
+                StartCoroutine ( "arrowDelay" );
             }
         }
-    }
+    }*/
 
     public IEnumerator arrowDelay () {
 
@@ -109,20 +119,15 @@ public class Enemy : Characters {
     }
 
     public void fletxes () {
+        float angle = 0;
         float rx = GameManager.Instance.Knight.transform.position.x;
         float ry = GameManager.Instance.Knight.transform.position.y;
-        Vector3 aux2 = new Vector3 ();
-        if ( rx > transform.position.x && ry > transform.position.y )
-            aux2 = new Vector3 ( transform.position.x + 1, transform.position.y + 1, 0 );
-        else if ( rx > transform.position.x && ry <= transform.position.y )
-            aux2 = new Vector3 ( transform.position.x + 1, transform.position.y - 1, 0 );
-        else if ( rx <= transform.position.x && ry > transform.position.y )
-            aux2 = new Vector3 ( transform.position.x - 1, transform.position.y + 1, 0 );
-        else if ( rx <= transform.position.x && ry <= transform.position.y )
-            aux2 = new Vector3 ( transform.position.x - 1, transform.position.y - 1, 0 );
-        
-        Instantiate ( arrow, aux2, transform.rotation );
-        arrow.rigidbody2D.AddForce ( new Vector2 ( rx, ry ) );
+        if ( rx != 0.0 || ry != 0.0 )
+            angle = Mathf.Atan2 ( ry, rx ) * Mathf.Rad2Deg;
+        Instantiate ( Khame, new Vector3 ( GameManager.Instance.Knight.transform.position.x, GameManager.Instance.Knight.transform.position.y - 2, GameManager.Instance.Knight.transform.position.z ), Quaternion.AngleAxis ( 90.0f - angle, Vector3.forward ) );
+        Vector2 him = new Vector2 ( GameManager.Instance.Knight.transform.position.x, GameManager.Instance.Knight.transform.position.y );
+        Khame.gameObject.rigidbody2D.AddForce ( him );
+        currentInputState = Enums.inputState_nm.WALK;
     }
 
     // VA BÉ
